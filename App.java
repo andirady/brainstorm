200662058c6f1e6723582eb1f26bb0562a80ecbe
andirady <kurniasan@yahoo.com.my>
2024-06-09 13:01:42 +0800

    Create App.java

---
+++ App.java
@@ -0,0 +1,18 @@
+@View("""
+      <div>{x} * 2 = {y}</div>
+
+      <button onclick={incrementX}>Increment</button>
+      <button onclick={decrementX}>Decrement</button>
+      """)
+public class App {
+    int x = 0;
+    @Derrived int y = x * 2;
+
+    void incrementX() {
+        x++;
+    }
+
+    void decrementX() {
+        x--;
+    }
+}
